338ce6e898183f4b88019e2a38b6f1b603641f2c
Adnaan Janees <adnaanjanees0@gmail.com>
2024-12-04 12:22:38 +0530

    page setup

---
+++ myportfolio/app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div><button className='bg-background card-shadow p-3 md:hidden'></button></div>
+  )
+}
+
+export default Navbar
