4548dd071a4d5652f619a8a5e3517ca45e79fdb8
Adnaan Janees <adnaanjanees0@gmail.com>
2024-12-04 22:11:18 +0530

    project section done

--- myportfolio/app/components/Sections.tsx
+++ myportfolio/app/components/Sections.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import Skills from './Skills/Skills'
+import Projects from './Projects/Projects'
 
 
 
@@ -9,6 +10,7 @@ const Sections = () => {
     <main className='flex flex-col gap-[142px] w-full md:max-w-screen-lg pt-[236px] md:pt-60 mx-auto'>
         <Hero />
         <Skills />
+        <Projects />
     </main>
   )
 }
